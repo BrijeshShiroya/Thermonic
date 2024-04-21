8473768efb646084df980344be4f1a29406d39b0
Brijesh Shiroya <brijesh281994@gmail.com>
2024-04-21 09:26:10 +0530

    Added add category screen

--- src/modules/Technical/AddCategoryScreen.js
+++ src/modules/Technical/AddCategoryScreen.js
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View } from 'react-native';
-import styles from './styles/TechnicalHomeScreenStyles';
-import { CustomBackground, CustomHeader } from '../../components';
+import styles from './styles/AddCategoryScreenStyle';
+import { CustomBackground, CustomButton, CustomHeader, CustomTextInput } from '../../components';
+import strings from '../../constants/Strings';
 
 const AddCategoryScreen = ({ navigation, route }) => {
 
     const title = route?.params?.categoryType == 1 ? 'Add Category' : 'Add Sub Category'
+    const [categoryName, setCategoryName] = useState('')
 
     const onBackPress = () => {
         navigation.goBack();
+    }
+
+    const onAddCategoryPress = () => {
+        alert('onAddCategoryPress')
     }
 
     return (
         <View style={styles.mainContainer}>
             <CustomHeader centerEnable={false} isTitle title={title} leftEnable onLeftPress={onBackPress} />
             <CustomBackground>
-                <Text>{title}</Text>
+                <View style={styles.innerContainer}>
+                    <Text style={styles.placeholder}>{strings.category}</Text>
+                    <CustomTextInput
+                        value={categoryName}
+                        placeholder={strings.category}
+                        keyboardType={'email-address'}
+                        containerStyle={styles.categotyContainer}
+                        onChangeText={text => setCategoryName(text)}
+                    />
+                </View>
+                <CustomButton
+                    title={strings.addCategoryButton}
+                    style={styles.addCategoryButton}
+                    onPress={onAddCategoryPress}
+                />
             </CustomBackground>
         </View>
     );
--- src/modules/Technical/styles/AddCategoryScreenStyle.js
+++ src/modules/Technical/styles/AddCategoryScreenStyle.js
@@ -1,6 +1,9 @@
 import { StyleSheet } from 'react-native';
 import {
     ApplicationStyles,
+    Colors,
+    Fonts,
+    scale,
     verticalScale
 } from '../../../theme';
 
@@ -10,7 +13,26 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingTop: verticalScale(8),
         paddingHorizontal: verticalScale(16),
+    },
+    innerContainer: {
+        flex: 1,
+        padding: scale(20),
+    },
+    placeholder: {
+        marginLeft: 5,
+        fontFamily: Fonts.type.semiBold,
+        color: Colors.textDark,
+        fontSize: Fonts.size.medium,
+        alignSelf: 'flex-start',
+    },
+    categoryContainer: {
+        marginBottom: 10
+    },
+    addCategoryButton: {
+        marginTop: verticalScale(30),
+        margin: scale(20),
     }
+
 });
 
 export default styles;
